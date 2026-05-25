528bb0b022327dfd10b28e3633af3e319298b651
webique-technology <webiquetech@gmail.com>
2026-05-25 13:16:36 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://admin.mahakumbhtourstravelsnashik.com','https://mahakumbhtourstravelsnashik.com','http://localhost:5173','http://192.168.1.5:5173','http://localhost:3000'],
+    'allowed_origins' => ['https://admin.mahakumbhtourstravelsnashik.com','https://mahakumbhtourstravelsnashik.com','http://localhost:5173','http://192.168.1.5:5173','http://localhost:3000','http://192.168.1.7:3000/'],
 
     'allowed_origins_patterns' => [],
 
